8814c85771847a44f24491e1dbcf944d9e60dd1c
rupesh palve <rupesh.palve48@outlook.com>
2022-11-28 17:37:40 -0500

    Add project files.

---
+++ Connected Architecture Demo/Emp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connected_Architecture_Demo
+{
+    internal class Emp
+    {
+        public string Name { get; set; }
+        public decimal Salary { get; set; }
+
+    }
+}
---
+++ Connected Architecture Demo/Form1.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+
+
+namespace Connected_Architecture_Demo
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataReader dr;
+        public Form1()
+        {
+            InitializeComponent();
+            string constr = ConfigurationManager.ConnectionStrings["defaultConnection"].ConnectionString;
+            con = new SqlConnection(constr);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string qry = "update Emp set name=@name,salary=@salary where id=@id";
+                cmd = new SqlCommand(qry, con);
+                cmd.Parameters.AddWithValue("@name", txtName.Text);
+                cmd.Parameters.AddWithValue("@salary", Convert.ToDecimal(txtSalary.Text));
+                cmd.Parameters.AddWithValue("@id", Convert.ToInt32(txtId.Text));
+                con.Open();
+                int result = cmd.ExecuteNonQuery();
+                if (result == 1)
+                {
+                    MessageBox.Show("Record updated");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                string qry = "delete from Emp where id=@id";
+                cmd = new SqlCommand(qry, con);
+                cmd.Parameters.AddWithValue("@id", Convert.ToInt32(txtId.Text));
+                con.Open();
+                int result = cmd.ExecuteNonQuery();
+                if (result == 1)
+                {
+                    MessageBox.Show("Record deleted");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+
+        }
+
+        //private void btnSearch_Click(object sender, EventArgs e)
+        //{
+        //    try
+        //    {
+        //        string qry = "select * from emp where id=@id";
+        //        cmd = new SqlCommand(qry, con);
+        //        cmd.Parameters.AddWithValue("@id", Convert.ToInt32(txtId.Text));
+        //        con.Open();
+        //        dr = cmd.ExecuteReader();
+        //        if (dr.HasRows)
+        //        {
+        //            while (dr.Read())
+        //            {
+        //                txtName.Text = dr["name"].ToString();
+        //                txtSalary.Text = dr["salary"].ToString();
+        //            }
+        //        }
+        //        else
+        //        {
+        //            MessageBox.Show("Record not found");
+        //        }
+
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MessageBox.Show(ex.Message);
+        //    }
+        //    finally
+        //    {
+        //        con.Close();
+        //    }
+
+        //}
+
+        private void btnShowAll_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string qry = "select * from emp";
+                cmd = new SqlCommand(qry, con);
+                con.Open();
+                dr = cmd.ExecuteReader();
+                if (dr.HasRows)
+                {
+                    DataTable dt = new DataTable();
+                    dt.Load(dr);
+                    dataGridView1.DataSource = dt;
+                }
+                else
+                {
+                    MessageBox.Show("Record not found");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string qry = "insert into empl values(@name,@salary)";
+                cmd = new SqlCommand(qry, con);
+                cmd.Parameters.AddWithValue("@name", txtName.Text);
+                cmd.Parameters.AddWithValue("@salary", Convert.ToDecimal(txtSalary.Text));
+                con.Open();
+                int result = cmd.ExecuteNonQuery();
+                if (result == 1)
+                {
+                    MessageBox.Show("Record inserted");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void Update_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string qry = "update Emp set name=@name,salary=@salary where id=@id";
+                cmd = new SqlCommand(qry, con);
+                cmd.Parameters.AddWithValue("@name", txtName.Text);
+                cmd.Parameters.AddWithValue("@salary", Convert.ToDecimal(txtSalary.Text));
+                cmd.Parameters.AddWithValue("@id", Convert.ToInt32(txtId.Text));
+                con.Open();
+                int result = cmd.ExecuteNonQuery();
+                if (result == 1)
+                {
+                    MessageBox.Show("Record updated");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string qry = "delete from Emp where id=@id";
+                cmd = new SqlCommand(qry, con);
+                cmd.Parameters.AddWithValue("@id", Convert.ToInt32(txtId.Text));
+                con.Open();
+                int result = cmd.ExecuteNonQuery();
+                if (result == 1)
+                {
+                    MessageBox.Show("Record deleted");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+
+        }
+
+        //private void SaveAll_Click(object sender, EventArgs e)
+        //{
+            //try
+            //{
+            //    string qry = "select * from emp";
+            //    cmd = new SqlCommand(qry, con);
+            //    con.Open();
+            //    dr = cmd.ExecuteReader();
+            //    if (dr.HasRows)
+            //    {
+            //        DataTable dt = new DataTable();
+            //        dt.Load(dr);
+            //        dataGridView1.DataSource = dt;
+            //    }
+            //    else
+            //    {
+            //        MessageBox.Show("Record not found");
+            //    }
+
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show(ex.Message);
+            //}
+            //finally
+            //{
+            //    con.Close();
+            //}
+
+
+       // }
+
+        private void Search_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string qry = "select * from emp where id=@id";
+                cmd = new SqlCommand(qry, con);
+                cmd.Parameters.AddWithValue("@id", Convert.ToInt32(txtId.Text));
+                con.Open();
+                dr = cmd.ExecuteReader();
+                if (dr.HasRows)
+                {
+                    while (dr.Read())
+                    {
+                        txtName.Text = dr["name"].ToString();
+                        txtSalary.Text = dr["salary"].ToString();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Record not found");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string qry = "insert into empl values(@name,@salary)";
+                cmd = new SqlCommand(qry, con);
+                cmd.Parameters.AddWithValue("@name", txtName.Text);
+                cmd.Parameters.AddWithValue("@salary", Convert.ToDecimal(txtSalary.Text));
+                con.Open();
+                int result = cmd.ExecuteNonQuery();
+                if (result == 1)
+                {
+                    MessageBox.Show("Record inserted");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+}
+
+
